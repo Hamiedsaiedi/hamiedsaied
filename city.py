f442855f9a0bdc9f359fd35bd6cf49487387adec
Hamiedsaiedi <157602806+Hamiedsaiedi@users.noreply.github.com>
2024-01-30 21:24:33 +0300

    Add files via upload

---
+++ city.py
@@ -0,0 +1,5 @@
+import datetime
+open("output.txt","w+")
+print(f"\t\tISLAMIC UNIVERSITY IN UGANDA\n\t\t\tKAMPALA CAMPUS\nNAME:\t\tHAMIED SAIEDI\nREG NO:\t\t223-063011-27359n\nLECTURER:\tMR.HAKIM\nDATE:\t\t\(f'Date:\tdatetime.date.today\nCOURSE UNIT:\tINTRODUCTION TO PROGRAMMING\t\t\t|")
+
+
